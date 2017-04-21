7d18cb99d0d0beb79c4ee7af97798d465bc61933
Lance_xu <yan.xu@uebuycar.com>
2017-04-21 15:08:29 +0800

    错误

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,7 +7,7 @@
             <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
             <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
             <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-            <br>
+            <br/>
             <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
         </ul>
         <h2>Ecosystem</h2>
