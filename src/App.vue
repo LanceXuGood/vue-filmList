ef45795e7eab5388004b9030f596b605b4547996
Lance <LanceXuGood@gmailcom>
2017-08-21 18:45:39 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
             <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
             <el-breadcrumb-item :to="{ path: '/home' }">活动管理</el-breadcrumb-item>
             <el-breadcrumb-item :to="{ path: '/card' }">活动列表</el-breadcrumb-item>
-            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情1</el-breadcrumb-item>
         </el-breadcrumb>
         <router-view></router-view>
     </div>
