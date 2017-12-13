6fb1257cfa019ba0c9c145837b4cfc3a37694916
xuyan <xuyan@laidian360.com>
2017-12-13 18:11:43 +0800

    还原

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,19 @@
 <template>
     <div id="app">
-        <transition name='slide'>
-           <Slidebar v-show='show' @hideSlidebar='Hide' ></Slidebar>
-        </transition>
+        <header class="top-bar">静静电影网</header>
         <div class="container-scroll">
             <router-view></router-view>
         </div>
     </div>
 </template>
-
 <script>
-import Slidebar from './components/Slidebar.vue';
-import Bus from './bus.js'
     export default {
-        name: 'app',
-        components:{
-            Slidebar
-        },
-        data(){
-            return{
-                show : false
-            }
-        },
-         mounted() {
-            Bus.$on('showSlidebar',this.slideValue)
-         },
-         methods:{
-             slideValue(){
-                 this.show = !this.show;
-             },
-             Hide(){
-                 this.show = false
-             }
-         }
+        name: 'app'
     };
 </script>
 <style lang="scss">
 @import './assets/scss/index.scss';
 @import './assets/scss/variable.scss';
-
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -59,25 +34,14 @@ import Bus from './bus.js'
             background: $primary-color;
             line-height: 80px;
             color: #ffffff;
+            font-size: 26px;
             text-align: center;
-             @include font-dpr(20px);
         }
         .container-scroll{
             flex: 1;
             overflow-y: auto;
             -webkit-overflow-scrolling: touch;
-            margin-top: 100px;
         }
     }
-        .slide-enter-active ,.slide-leave-active,.slide-enter-active .slidebar_con ,.slide-leave-active .slidebar_con, {
-          transition: all .3s ease;
-        }
-        .slide-enter,.slide-leave-active{
-            opacity: 0;
-        }
-        .slide-enter .slidebar_con , .slide-leave-active .slidebar_con{
-          transform: translateX(-100%);
-          opacity: 0;
-        }
 </style>
 
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,19 +1,16 @@
 <template>
     <div class="home-page">
-        <headee></headee>
         <HomeListItem></HomeListItem>
     </div>
 </template>
 
 <script>
-    import Headee from '../components/Heade.vue';
     import HomeListItem from '../components/HomeListItem.vue';
     export default {
         name: '',
         props: {},
         components:{
-            HomeListItem,
-            Headee
+            HomeListItem
         },
         data() {
             return {
@@ -31,12 +28,13 @@
 <style lang="scss" scoped>
     @import '../assets/scss/variable.scss';
     .home-page {
-        @include font-dpr(14px);
+        font-size: 34px;
         h3 {
-            @include font-dpr(40px);
+            font-size: 40px;
             background-color: #f2f8f2;
             color: $sub-color;
             padding: 20px 0;
         }
+
     }
 </style>
